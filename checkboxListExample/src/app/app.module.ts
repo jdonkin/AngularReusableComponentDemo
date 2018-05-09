4abae22f0fe88045efb3a1142ac56bfbfc35564f
Jonathan <jonathan.donkin01@gmail.com>
2018-05-08 22:34:53 -0600

    added checkbox component

--- checkboxListExample/src/app/app.module.ts
+++ checkboxListExample/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { CheckboxComponent } from './checkbox/checkbox.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CheckboxComponent
   ],
   imports: [
     BrowserModule
